acbc3e3c63de441e0fc3c4572e1e59c9a1265aad
Emanuel <emanuel.facundo14@gmail.com>
2021-06-08 12:08:54 -0300

    04. Operações básicas (Primeira parte)

--- #3/index.ts
+++ #3/index.ts
@@ -1,3 +1,6 @@
+// Usada a extensão Quokka no VSCode para uma melhor visualização
+// 03. Aquecimento
+
 function countNeg(a: Array<number>) {
   return a.filter(x => x < 0).length
 }
@@ -48,3 +51,62 @@ function soma2(a: number, b: number) {
 }
 
 console.log(soma2(-4,6))
+
+
+// 04. Operações básicas
+
+function somaImpares(a: Array<number>) {
+  return a.filter(x => !(x % 2 === 0))
+    .reduce((soma, x) => soma + x)
+}
+
+console.log(somaImpares([3,2,4,6,5,7,8,0,1]))
+
+function max3(a: number, b: number, c: number) {
+  return a > b && a > c ? a :
+    b > a && b > c ? b : c
+}
+
+console.log(max3(6,7,9))
+
+function fatorial(a: number){
+  return a < 2 ? 1 : a * fatorial(a-1)
+}
+
+console.log(fatorial(5))
+
+function elemento(a: number, b: Array<number>) {
+  return a < 0 ? b[b.length+a] : b[a]
+}
+
+console.log(elemento(-2, [2,7,3,9]))
+
+function pertence(a: number, b: Array<number>) {
+  return b.includes(a)
+}
+
+console.log(pertence(2, [3,7,4,2]))
+
+function total(a: Array<number>) {
+  return a.pop() ? 1 + total(a) : 0
+}
+  
+console.log(total([5,2,4,5,6,1]))
+
+function maior(a: Array<number>) {
+  return a.reduce((maior, x) => maior < x ? x : maior)
+}
+
+console.log(maior([1,2,3,1,10]))
+
+function corpo(a: Array<number>) {
+  return a.length < 2 ? [] : a.slice(0, a.length - 1)
+}
+
+console.log(corpo([1,2,3,4]))
+
+function divide(a: Array<number>, b: number) {
+  return {tu:a.slice(0,b),  pla:a.slice(b, a.length)}
+}
+
+console.log(divide([1,2,3,4],2))
