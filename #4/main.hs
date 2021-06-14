ef235e4d5cd8e2bbfd256e20ce9f69a0731f888a
Emanuel <emanuel.facundo14@gmail.com>
2021-06-14 12:01:13 -0300

    Atividade #4 (segunda parte)

--- #4/main.hs
+++ #4/main.hs
@@ -24,8 +24,54 @@ maioresQue a b
   | otherwise = filter (\x -> x > a) b
 
 --  @021 concatena
-concatena a b = a ++ b
+concatena [] [] = []
+concatena [] b = b
+concatena a [] = a
+concatena (x : xs) ys = x : concatena xs ys
 
+--  @027 reverso
+reverso [] = []
+reverso xs = last xs : reverso (init xs)
 
+--  @029 intercal - intercalar duas listas
+intercal [] ys = ys
+intercal xs [] = xs
+intercal (x:xs) (y:ys) = x : y : intercal xs ys
 
+--  @032 sequencia
+sequencia 0 m = []
+sequencia n m = m : sequencia (n-1) (m+1)
 
+--  @037 rotEsq
+rotEsq 0 xs = xs
+rotEsq n (x:xs) = rotEsq (n-1) (xs++[x])
+
+--  @038 rotDir
+rotDir 0 xs = xs
+rotDir n xs = rotDir (n-1) ([(last xs)]++(init xs))
+
+--  @048 quadperf
+quad a b 
+    | ((a*a) > b) = False
+    | ((a*a) == b) = True
+    | otherwise = quad (a+1) b
+
+quadperf n = quad 2 n
+
+--  @059 listacc - lista acumulativa
+listacc [] = []
+listacc (x:xs) 
+  | xs == [] = [x]
+  | otherwise = x : listacc ([(x+(head xs))]++(tail xs))
+
+--  @061 line - linhas de um triângulo aritmético
+line n = [(fn)..((fn)+(n-1))]
+  where fn = ((sum [1..(n-1)])+1)
+
+--  @062 triangle - triângulo aritmético
+invert 0 = [] 
+invert n = line n : invert (n-1)
+
+triangle n = reverse (invert n)
+
+-- Ficou faltando as questões "menores","alter" e "deletee"
